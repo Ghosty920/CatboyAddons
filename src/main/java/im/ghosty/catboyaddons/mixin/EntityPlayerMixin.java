071ccbcc8967a852553e938e9c2518b7c3d8b9e8
Noamm <116365534+Noamm9@users.noreply.github.com>
2025-08-20 23:17:35 +0300

    Update EntityPlayerMixin.java

--- src/main/java/im/ghosty/catboyaddons/mixin/EntityPlayerMixin.java
+++ src/main/java/im/ghosty/catboyaddons/mixin/EntityPlayerMixin.java
@@ -13,6 +13,9 @@ import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraftforge.common.util.Constants;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 
 @SideOnly(Side.CLIENT)
 @Mixin(EntityPlayer.class)
@@ -32,11 +35,25 @@ public class EntityPlayerMixin {
 		if (itemInUse == null || inventory == null) return;
 		ItemStack item = inventory.getCurrentItem();
 		if (item == null || !(item.getItem() instanceof ItemBow)) return;
-		String id = ItemUtils.INSTANCE.getItemId(item);
-		if (!id.contains("SHORTBOW") && !id.contains("TERMINATOR") && !id.contains("ITEM_SPIRIT_BOW") && !id.contains("MOSQUITO_BOW") && !id.contains("MACHINE_GUN_BOW"))
-			return;
+		if (!catBoyAddons$isShortBow(item)) return;
 		itemInUse = null;
 		itemInUseCount = 0;
 	}
-	
+
+	@Unique
+	private boolean catBoyAddons$isShortBow(ItemStack item) {
+        if (item == null || !item.hasTagCompound()) return false;
+
+        NBTTagCompound display = item.getTagCompound().getCompoundTag("display");
+        if (!display.hasKey("Lore", Constants.NBT.TAG_LIST)) return false;
+
+        NBTTagList loreNBT = display.getTagList("Lore", Constants.NBT.TAG_STRING);
+
+        for (int i = 0; i < loreNBT.tagCount(); i++) {
+            String line = loreNBT.getStringTagAt(i);
+            if (line.contains("Shortbow: Instantly shoots!")) return true;
+        }
+
+        return false;
+    }
 }
